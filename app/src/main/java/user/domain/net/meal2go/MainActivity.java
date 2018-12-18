a7b5c957a6b078d79a5d0d4679f17c8fb206fadd
Damirkhan <d.aloev@student.inha.uz>
2018-12-18 21:43:41 +0500

    getting started 2

--- app/src/main/java/user/domain/net/meal2go/MainActivity.java
+++ app/src/main/java/user/domain/net/meal2go/MainActivity.java
@@ -1,22 +1,29 @@
 package user.domain.net.meal2go;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.ViewDragHelper;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import java.lang.reflect.Field;
 
 public class MainActivity extends AppCompatActivity
     implements NavigationView.OnNavigationItemSelectedListener,BottomNavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +37,49 @@ public class MainActivity extends AppCompatActivity
 
         setSupportActionBar(toolbar);
         drawerLayout = findViewById(R.id.drawer_layout);
+
+
+        Field mDragger = null;
+        try {
+            mDragger = drawerLayout.getClass().getDeclaredField("mLeftDragger");
+
+        mDragger.setAccessible(true);
+        ViewDragHelper draggerObj = (ViewDragHelper) mDragger.get(drawerLayout);
+
+        Field mEdgeSize = draggerObj.getClass().getDeclaredField("mEdgeSize");
+        mEdgeSize.setAccessible(true);
+        int edge = mEdgeSize.getInt(draggerObj);
+
+        mEdgeSize.setInt(draggerObj, edge * 10  );
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+
         ActionBarDrawerToggle toggle =
                 new ActionBarDrawerToggle(this, drawerLayout,
                         toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container,new menuFragment()).commit();
+
+        //control navigationView
+        navigationView = findViewById(R.id.nav_view);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        if(savedInstanceState==null){
+            navigationView.setNavigationItemSelectedListener(this);
+            navigationView.setCheckedItem(R.id.nav_menu);
+            bottomNavigationView.setOnNavigationItemSelectedListener(this);
+            bottomNavigationView.setSelectedItemId(R.id.nav_menu);
+        }
+
+
+
 
     }
 
@@ -52,22 +97,39 @@ public class MainActivity extends AppCompatActivity
             case R.id.nav_menu:
                 getSupportFragmentManager().beginTransaction()
                         .replace(R.id.fragment_container, new menuFragment()).commit();
+                bottomNavigationView.setVisibility(View.VISIBLE);
+                bottomNavigationView.setOnNavigationItemSelectedListener(null);
+                bottomNavigationView.setSelectedItemId(R.id.nav_menu);
+                bottomNavigationView.setOnNavigationItemSelectedListener(this);
+                navigationView.setCheckedItem(menuItem);
                 break;
             case R.id.nav_favorite:
                 getSupportFragmentManager().beginTransaction()
                         .replace(R.id.fragment_container, new favoriteFragment()).commit();
+                bottomNavigationView.setOnNavigationItemSelectedListener(null);
+                bottomNavigationView.setSelectedItemId(R.id.nav_favorite);
+                bottomNavigationView.setOnNavigationItemSelectedListener(this);
+                bottomNavigationView.setVisibility(View.VISIBLE);
+                navigationView.setCheckedItem(menuItem);
                 break;
             case R.id.nav_orders:
                 getSupportFragmentManager().beginTransaction()
                         .replace(R.id.fragment_container, new orderFragment()).commit();
+                bottomNavigationView.setVisibility(View.VISIBLE);
+                bottomNavigationView.setOnNavigationItemSelectedListener(null);
+                bottomNavigationView.setSelectedItemId(R.id.nav_orders);
+                bottomNavigationView.setOnNavigationItemSelectedListener(this);
+                navigationView.setCheckedItem(menuItem);
                 break;
             case R.id.nav_profile:
                 getSupportFragmentManager().beginTransaction()
                         .replace(R.id.fragment_container, new profileFragment()).commit();
+                bottomNavigationView.setVisibility(View.GONE);
                 break;
             case R.id.nav_about:
                 getSupportFragmentManager().beginTransaction()
                         .replace(R.id.fragment_container, new aboutFragment()).commit();
+                bottomNavigationView.setVisibility(View.GONE);
                 break;
         }
         drawerLayout.closeDrawer(GravityCompat.START);
@@ -75,5 +137,7 @@ public class MainActivity extends AppCompatActivity
     }
 
     public void CartPressAction(View view) {
+        Intent intent = new Intent(this,CartActivity.class);
+        startActivity(intent);
     }
 }
